61354307a5bfa5ce072ed7ebeb54632748c0697e
hangnguyen <nguyenhang37nth@gmail.com>
2024-05-16 00:54:20 +0700

    topic_09_custom_dropdown_p2

---
+++ selenium/javaTester/Topic_05_Parameter.java
@@ -0,0 +1,18 @@
+package javaTester;
+
+public class Topic_05_Parameter {
+     static String fullNameGlobal = "Automation Testing";
+
+    public static void main(String[] args) {
+        //setFullName ("Manual Testing");
+
+    }
+
+    public void setFullname(String fullName){
+        fullNameGlobal = fullName;
+    }
+    //public static string getFullName(){
+    //  return fullNameGlobal();
+
+    }
+
--- selenium/webdriver/Topic_09_custom_dropdown.java
+++ selenium/webdriver/Topic_09_custom_dropdown.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import net.bytebuddy.pool.TypePool;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,7 +23,7 @@ public class Topic_09_custom_dropdown {
     WebDriver driver;
     WebDriverWait explicitWait;
 
-    @BeforeClass
+@BeforeClass
         public void beforeClass() {
 
             driver = new FirefoxDriver();
@@ -32,13 +33,14 @@ public class Topic_09_custom_dropdown {
 
         }
 
-        @Test
+@Test
         public void TC_01_Register() {
            driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
            driver.findElement(By.cssSelector("span#number-button")).click();
             sleepInsecond(3);
 
-            explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("ul#number-menu div")));
+            explicitWait.until(ExpectedConditions.
+                    presenceOfAllElementsLocatedBy(By.cssSelector("ul#number-menu div")));
 
             List< WebElement> allItems= driver.findElements(By.cssSelector("ul#number-menu div"));
               for(WebElement item:allItems){
@@ -53,14 +55,142 @@ public class Topic_09_custom_dropdown {
         }
 
 
-        @Test
-        public void TC_02_Login() {
+@Test
+        public void TC_02_Jquery() {
+            driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
+
+            selectItemInDropdown("span#speed-button","ul#speed-menu div","Fast");
+            sleepInsecond(2);
+
+            selectItemInDropdown("span#files-button","ul#files-menu div","jQuery.js");
+            sleepInsecond(2);
+
+
+            selectItemInDropdown("span#number-button","ul#number-menu div","10");
+            sleepInsecond(2);
+
+            selectItemInDropdown("span#salutation-button","ul#salutation-menu div","Mrs.");
+            sleepInsecond(2);
+
+            Assert.assertEquals(driver.findElement(By.cssSelector("span#speed-button>span.ui-selectmenu-text")).getText(),"Fast");
+
+            Assert.assertEquals(driver.findElement(By.cssSelector("span#files-button>span.ui-selectmenu-text")).getText(),"jQuery.js");
+
+            Assert.assertEquals(driver.findElement(By.cssSelector("span#number-button>span.ui-selectmenu-text")).getText(),"10");
+
+            Assert.assertEquals(driver.findElement(By.cssSelector("span#salutation-button>span.ui-selectmenu-text")).getText(),"Mrs.");
+
+
+
         }
-        public void sleepInsecond (long timeInSecond){
-            try {
-                Thread.sleep(timeInSecond * 1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+@Test
+        public void TC_03_React(){
+         driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+         selectItemInDropdown("i.dropdown.icon","div.item>span.text","Matt");
+         Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(),"Matt");
+         sleepInsecond(3);
+
+         selectItemInDropdown("i.dropdown.icon","div.item>span.text","Christian");
+         Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(),"Christian");
+         sleepInsecond(3);
+
+         selectItemInDropdown("i.dropdown.icon","div.item>span.text","Jenny Hess");
+         Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(),"Jenny Hess");
+         sleepInsecond(3);
+
+        }
+
+
+@Test
+    public void TC_04_Vuejs(){
+        driver.get("https://mikerodham.github.io/vue-dropdowns/");
+        selectItemInDropdown("li.dropdown-toggle","ul.dropdown-menu a","First Option");
+        Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(),"First Option");
+        sleepInsecond(3);
+
+        selectItemInDropdown("li.dropdown-toggle","ul.dropdown-menu a","Third Option");
+        Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(),"Third Option");
+        sleepInsecond(3);
+
+
+    }
+
+@Test
+    public void TC_05_Editable(){
+        driver.get("https://react.semantic-ui.com/maximize/dropdown-example-search-selection/");
+        selectItemEditableInDropdown("input.search","div.selected.item","Belgium");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(),"Belgium");
+        sleepInsecond(3);
+
+        selectItemEditableInDropdown("input.search","div.selected.item","Bangladesh");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(),"Bangladesh");
+        sleepInsecond(3);
+
+        selectItemEditableInDropdown("input.search","div.selected.item","American Samoa");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(),"American Samoa");
+        sleepInsecond(3);
+
+    }
+
+    @Test
+    public void TC_06_Default_NopEcommerce(){
+        driver.get("https://demo.nopcommerce.com/register");
+        selectItemInDropdown("select[name='DateOfBirthDay']","select[name='DateOfBirthDay']>option","15");
+        Assert.assertTrue(driver.findElement(By.cssSelector("select[name='DateOfBirthDay']>option[value='15']")).isSelected());
+        sleepInsecond(3);
+
+        selectItemInDropdown("select[name='DateOfBirthMonth']","select[name='DateOfBirthMonth']>option","August");
+        Assert.assertTrue(driver.findElement(By.cssSelector("select[name='DateOfBirthMonth']>option[value='8']")).isSelected());
+        sleepInsecond(3);
+
+        selectItemInDropdown("select[name='DateOfBirthYear']","select[name='DateOfBirthYear']>option","1997");
+        Assert.assertTrue(driver.findElement(By.cssSelector("select[name='DateOfBirthYear']>option[value='1997']")).isSelected());
+        sleepInsecond(3);
+
+
+
+
+
+
+
+
+    }
+
+@AfterClass
+        public void afterClass(){driver.quit();}
+
+    public void sleepInsecond (long timeInSecond){
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+       public void selectItemInDropdown(String parentCss,String childItemCss, String itemTextExpected )
+       {
+           driver.findElement(By.cssSelector(parentCss)).click();
+           List<WebElement >allItems = explicitWait.until(ExpectedConditions.
+                   presenceOfAllElementsLocatedBy(By.cssSelector(childItemCss)));
+           for (WebElement item : allItems) {
+               if (item.getText().equals(itemTextExpected)){
+                   item.click();
+                   break;
+               }
+           }
+       }
+    public void selectItemEditableInDropdown(String parentCss,String childItemCss, String itemTextExpected ){
+        driver.findElement(By.cssSelector(parentCss)).clear();
+        driver.findElement(By.cssSelector(parentCss)).sendKeys(itemTextExpected);
+        List<WebElement >allItems = explicitWait.until(ExpectedConditions.
+                presenceOfAllElementsLocatedBy(By.cssSelector(childItemCss)));
+        for (WebElement item : allItems) {
+            if (item.getText().equals(itemTextExpected)){
+                item.click();
+                break;
             }
         }
     }
+
+
+
+}
